bc9165c66f8d6a4e820f9e16793950226b17431c
Dravd Brown <101314042@georgebrown.ca>
2025-01-18 21:13:57 -0500

    Hello World

---
+++ demo.py
@@ -0,0 +1,20 @@
+#sample coding questions 01 week 01
+#Dravid Brown
+
+arr = {1,4,7,9}
+
+
+a=1
+b=2
+c=3
+d=4
+e = ((a - ((b ** c) // d)) + (a % c))
+print(e)
+
+
+temperature=32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+
+userAge = input("PLease enter your age: ")
+print("Now showing the shop items filtered by age: ", userAge)
